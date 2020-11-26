dba4faaa17e9f2673ae4d639fc93ed364ca002f9
Abdul Khan <akhan@lcs.on.ca>
2020-11-26 09:48:42 -0500

    Completed BowWOW app! Now shows new image whenever button is pressed

---
+++ BowWOW/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDog.swift
+//  BowWOW
+//
+//  Created by Abdul Ahad Khan on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
